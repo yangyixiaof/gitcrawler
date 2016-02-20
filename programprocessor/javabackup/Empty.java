d613164f0f8955fb49650a0940e3389a82413d69
yyx <yyx@ubuntu>
2016-02-20 05:16:13 -0800

    small change.

---
+++ programprocessor/javabackup/Empty.java
@@ -0,0 +1,15 @@
+package cn.yyx.research.language.Utility;
+
+public class TTest extends ParentTest{
+	
+	class TTest2
+	{
+		
+	}
+	
+}
+
+class tt 
+{
+	int a;
+}
--- programprocessor/src/main/java/cn/yyx/research/language/JDTHelper/ClassLogicDetailCorpus.java
+++ programprocessor/src/main/java/cn/yyx/research/language/JDTHelper/ClassLogicDetailCorpus.java
@@ -7,8 +7,17 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeMemberDeclaration;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 import cn.yyx.research.language.Utility.CorpusContentPair;
 import cn.yyx.research.language.simplified.JDTHelper.SimplifiedCodeGenerateASTVisitor;
@@ -18,10 +27,14 @@ public class ClassLogicDetailCorpus {
 	@SuppressWarnings("unchecked")
 	public static ArrayList<CorpusContentPair> GenerateClassDetailCorpus(CompilationUnit compilationUnit)
 	{
-		List<ASTNode> typeDeclarations = compilationUnit.types();
+		List<AbstractTypeDeclaration> typeDeclarations = compilationUnit.types();
 		Map<String, String> allcodemap = new TreeMap<String, String>();
-		for (Object object : typeDeclarations) {
-			ASTNode clazzNode = (ASTNode) object;
+		for (AbstractTypeDeclaration object : typeDeclarations) {
+			AbstractTypeDeclaration clazzNode = (AbstractTypeDeclaration) object;
+			if (IsEmptyTypeDeclaration(clazzNode))
+			{
+				continue;
+			}
 			SimplifiedCodeGenerateASTVisitor fmastv = new SimplifiedCodeGenerateASTVisitor();
 			clazzNode.accept(fmastv);
 			Map<String, String> codemap = fmastv.GetGeneratedCode();
@@ -54,6 +67,33 @@ public class ClassLogicDetailCorpus {
 		// clear variables
 		allcodemap = null;
 		return result;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static boolean IsEmptyTypeDeclaration(AbstractTypeDeclaration clazzNode)
+	{
+		boolean isempty = true;
+		List<BodyDeclaration> bs = clazzNode.bodyDeclarations();
+		Iterator<BodyDeclaration> itr = bs.iterator();
+		while (itr.hasNext())
+		{
+			BodyDeclaration bd = itr.next();
+			if (bd instanceof AnnotationTypeMemberDeclaration || bd instanceof EnumConstantDeclaration || bd instanceof FieldDeclaration || bd instanceof Initializer || bd instanceof MethodDeclaration)
+			{
+				isempty = false;
+				break;
+			}
+			if (bd instanceof AnnotationTypeDeclaration || bd instanceof EnumDeclaration || bd instanceof TypeDeclaration)
+			{
+				boolean resempty = IsEmptyTypeDeclaration((AbstractTypeDeclaration)bd);
+				if (!resempty)
+				{
+					break;
+				}
+	 			
+			}
+		}
+		return isempty;
 	}
 	
 }
