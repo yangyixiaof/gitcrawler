7d7e4357ceaac42a6230ee9f90b76dab749ecdac
yyx <yangyixiaofirst@163.com>
2016-02-26 00:38:56 +0800

    small change.

--- programprocessor/javabackup/Empty.java
+++ programprocessor/javabackup/Empty.java
@@ -2,6 +2,30 @@ package cn.yyx.research.language.Utility;
 
 public class TTest extends ParentTest{
 	
+	public void HeiHei()
+	{
+		new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("RT");
+				new Runnable() {
+					@Override
+					public void run() {
+						System.out.println("run");
+						new Runnable() {
+							@Override
+							public void run() {
+								System.out.println("run12345");
+							}
+						};
+						System.out.println("qwer");
+					}
+				};
+				System.out.println("OIU");
+			}
+		};
+	}
+	
 	class TTest2
 	{
 		
