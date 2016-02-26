a53f44984cecbc842fa7dac9e00179000b871750
yyx <yangyixiaofirst@163.com>
2016-02-26 12:08:13 +0800

    small change.

--- programprocessor/javatest/AnonymousTest.java
+++ programprocessor/javatest/AnonymousTest.java
@@ -9,7 +9,7 @@ public class Empty {
 			public void run() {
 				System.out.println("RT");
 				new Runnable() {
-					/*@Override
+					@Override
 					public void run() {
 						System.out.println("run");
 						new Runnable() {
@@ -19,7 +19,7 @@ public class Empty {
 							}
 						};
 						System.out.println("qwer");
-					}*/
+					}
 				};
 				System.out.println("OIU");
 			}
