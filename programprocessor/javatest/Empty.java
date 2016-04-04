6739a0a0e211f8193e52d874611d39871b80b829
yyx <yangyixiaofirst@163.com>
2016-04-04 20:56:56 +0800

    small change.

--- programprocessor/javatest/Empty.java
+++ programprocessor/javatest/Empty.java
@@ -4,15 +4,11 @@ public class Empty {
 	
 	public void HeiHei()
 	{
-		double a = 45/(1+4/2*5+a);
+		h().m(hh().a(hhh().b()));
+		// double a = 45/(1+4/2*5+a);
 		// a[1+5+6] = 0;
 		// int a = 0;
 		// a = n(m()+100);
-		Runnable r = new Runnable() {
-			@Override
-			public void run() {
-			}
-		};
 	}
 	
 	/*public int n(int p)
