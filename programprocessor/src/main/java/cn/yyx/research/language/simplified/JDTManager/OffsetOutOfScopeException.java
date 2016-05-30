8c16545960daf26cc383fb3d8c86c3901d27e355
yyx <yangyixiaofirst@163.com>
2016-05-30 19:42:25 +0800

    small change.

---
+++ programprocessor/src/main/java/cn/yyx/research/language/simplified/JDTManager/OffsetOutOfScopeException.java
@@ -0,0 +1,11 @@
+package cn.yyx.research.language.simplified.JDTManager;
+
+public class OffsetOutOfScopeException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public OffsetOutOfScopeException(String info) {
+		super(info);
+	}
+	
+}
