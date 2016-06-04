a9fa22f3a50954911e8dc66b2fbeb09ae7927401
yyx <yangyixiaofirst@163.com>
2016-06-04 15:30:24 +0800

    small change.

--- programprocessor/src/main/java/cn/yyx/research/language/JDTManager/OneJavaFileAnonymousClassesCode.java
+++ programprocessor/src/main/java/cn/yyx/research/language/JDTManager/OneJavaFileAnonymousClassesCode.java
@@ -30,6 +30,18 @@ public class OneJavaFileAnonymousClassesCode extends JavaCode {
 		AddOneNodeCode(nc);
 	}
 	
+	public ArrayList<String> toList()
+	{
+		ArrayList<String> result = new ArrayList<String>();
+		if (codes.size() == 0 || (codelist.size() > 0 && !codes.get(0).equals(codelist.get(0))))
+		{
+			result.addAll(codelist);
+			result.addAll(codes);
+			return result;
+		}
+		return super.toList();
+	}
+	
 	public void OneSentenceEnd()
 	{
 		/*System.err.println(sb);
