760657724eb379c193291f5ad01047af61444b76
yyx <yyx@ubuntu>
2017-05-10 23:22:20 -0700

    small change.

--- programprocessor/src/main/java/cn/yyx/research/language/programprocessor/ProgramHandle.java
+++ programprocessor/src/main/java/cn/yyx/research/language/programprocessor/ProgramHandle.java
@@ -69,7 +69,7 @@ public class ProgramHandle {
 	}
 
 	public static void main(String[] args) {
-		String processdir = "";
+		String processdir = "experiment-dataset/TrainingData";
 		ProgramHandle app = null;
 		String cmd = null;
 		boolean stop = false;
