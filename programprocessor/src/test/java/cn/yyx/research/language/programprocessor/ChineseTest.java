5e096f678efdd769157f006fe206309fb95e42ae
yyx <yyx@ubuntu>
2019-08-19 23:54:59 -0700

    small change.

---
+++ programprocessor/src/test/java/cn/yyx/research/language/programprocessor/ChineseTest.java
@@ -0,0 +1,11 @@
+package cn.yyx.research.language.programprocessor;
+
+public class ChineseTest {
+	
+	public void Ha() {
+		String b = "似的似的三到四扼腕她向你哇阿三但的撒的撒";
+		String a = b + "哦破哦其外阿斯惹";
+		System.out.println(a);
+	}
+	
+}
